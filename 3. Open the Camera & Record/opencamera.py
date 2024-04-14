de9dfcb96ae6fdb30b2043ab37298babf9d75d16
Yusuf Asım Arslan <84157646+ranuelyn@users.noreply.github.com>
2024-04-15 02:00:44 +0300

    Add files via upload

---
+++ 3. Open the Camera & Record/opencamera.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 15 16:34:32 2023
+
+@author: yusuf
+"""
+
+import cv2
+
+# capture
+cap = cv2.VideoCapture(0)
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+print(width, height)
+
+# video kaydet
+
+writer = cv2.VideoWriter('test2.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Video', frame)
+    
+    #save
+    writer.write(frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+cap.release()
+writer.release()
+cv2.destroyAllWindows()
